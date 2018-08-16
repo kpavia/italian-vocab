cae85e0b1af26a4c26447a89371f89050125dbbc
kpavia <kennethpavia@gmail.com>
2018-08-16 15:42:43 -0600

    added more word banks

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-words = {"floor": "pavimento", "ceiling": "soffitto", "shelf": "scaffale", "wall": "parete", "window": "finestra",
-         "door": "porta", "chair": "sedia", "armchair": "poltrona", "couch": "divano", "carpet": "moquette",
-         "cushions": "cuscini", "coffee table": "tavolino", "rug": "tappeto", "bed": "letto", "room": "stanza"}
+room_items = {"floor": "pavimento", "ceiling": "soffitto", "shelf": "scaffale", "wall": "parete", "window": "finestra",
+              "door": "porta", "chair": "sedia", "armchair": "poltrona", "couch": "divano", "carpet": "moquette",
+              "cushions": "cuscini", "coffee table": "tavolino", "rug": "tappeto", "bed": "letto", "room": "stanza",
+              "bathroom sink": "lavabo", "vacuum": "aspirapolvere"}
+
+kitchen_items = {"kitchen": "cucina", "kitchen sink": "lavello", "fork": "forchetta", "spoon": "cuchiaio",
+                 "knife": "coltello", "cup": "tazza", "plate": "piatto", "oven": "forno", "stove": "fornelli",
+                 "refrigerator": "frigorifero", "microwave": "microonde", "sauce pan": "padella", "pot": "pentola"}
+
+common_food_items = {"garlic": "aglio", "oil": "olio", "basil": "basilico", "cheese": "formaggio",
+                     "mozzarella": "mozzarella", "parmesan": "parmigiano", "tomato": "pomodoro", "sauce": "salsa",
+                     "vegetables": "verdura", "fruit": "frutta", "bread": "pane", "grain": "cereali"}
 
 
-def quiz():
+def room_quiz():
     correct = 0
     wrong = 0
     print("Translate the word into Italian.")
-    for item in words:
+    for item in room_items:
         question = input(f'{item} in Italian is:\n').lower()
-        if words[item] == question:
+        if room_items[item] == question:
             print("Correct!")
             correct += 1
         else:
-            print(f'Wrong. Answer is {words[item]}')
+            print(f'Wrong. Answer is {room_items[item]}')
             wrong += 1
             i = 0
             while i < 10:
                 i += 1
                 review = input(f'{item} in Italian is:\n').lower()
-                if words[item] == review:
+                if room_items[item] == review:
                     print("Correct!")
                 else:
-                    print(f'Wrong. Answer is {words[item]}')
+                    print(f'Wrong. Answer is {room_items[item]}')
     else:
         print(f'Results\n'
               f'Correct: {correct}\n'
               f'Wrong: {wrong}\n'
-              f'Total: {len(words)}')
+              f'Total: {len(room_items)}')
 
 
-quiz()
+
 
