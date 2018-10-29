a6f041d458f433f4d8ef49eb4e9186df078c0929
kpavia <kennethpavia@gmail.com>
2018-10-29 08:18:52 -0600

    added indirect object pronouns, direct object pronouns, ordinal numbers

--- main.py
+++ main.py
@@ -13,6 +13,27 @@ food_items = {"garlic": "aglio", "oil": "olio", "basil": "basilico", "cheese": "
               "vegetables": "verdura", "fruit": "frutta", "bread": "pane", "grain": "cereali", "liquid": "liquido",
               "powder": "polvere", "cream": "crema"}
 
+ordinal_numbers = {"first": "primo", "second": "secondo", "third": "terzo", "fourth": "quarto", "fifth": "quinto",
+                   "sixth": "sesto", "seventh": "septimo", "eighth": "ottavo", "ninth": "nono", "tenth": "decimo",
+                   "eleventh": "undicesimo", "twelfth": "dodicesimo", "thirteenth": "tredicesimo",
+                   "fourteenth": "quattordicesimo", "fifteenth": "quindicesimo", "sixteenth": "sedicesimo",
+                   "seventeenth": "diciasettesimo", "eighteenth": "diciottesimo", "ninteenth": "diciannovesimo",
+                   "twentieth": "ventesimo", "twenty-first": "ventunesimo", "twenty-second": "ventiduesimo",
+                   "twenty-third": "ventitresimo", "twenty-fourth": "ventiquattresimo",
+                   "twenty-fifth": "venticinquesimo", "twenty-sixth": "ventiseiesimo",
+                   "twenty-seventh": "ventisettesimo", "twenty-eighth": "ventiottesimo",
+                   "twenty-ninth": "ventinovesimo", "thirtieth": "trentesimo", "fourtieth": "quarantesimo",
+                   "fiftieth": "cinquantesimo", "sixtieth": "sessantesimo", "seventieth": "settantesimo",
+                   "eightieth": "ottantesimo", "nintieth": "novantesimo", "hundredth": "centesimo"}
+
+direct_object_pronouns = {"1st person singular": "mi", "2nd person singular": "ti", "3rd person singular masc.": "lo",
+                          "3rd person singular fem.": "la", "1st person plural": "ci", "2nd person plural": "vi",
+                          "3rd person plural masc.": "li", "3rd person plural fem.": "le"}
+
+indirect_object_pronouns = {"1st person singular": "mi", "2nd person singular": "ti",
+                            "3rd person singular masc.": "gli", "3rd person singular fem.": "le",
+                            "1st person plural": "ci", "2nd person plural": "vi", "3rd person plural": "loro"}
+
 
 def room_quiz():
     correct = 0
